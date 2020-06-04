536b8c822e6db825c3792c51f938ff0ea51c0e44
KyleCho <kyle@bringprice.com>
2020-06-04 10:32:00 +0900

    delete domain

--- src/main/java/com/kyle/elasticsearchpractice/persistence/AgodaHotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kyle.elasticsearchpractice.persistence;
-
-import com.kyle.elasticsearchpractice.domain.AgodaHotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgodaHotelRepository extends JpaRepository<AgodaHotel,Long> {
-}
